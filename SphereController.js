eb9297c310d15e497786f7bdbc12a0999e910d26
naomi choi <fhdjskal2@naver.com>
2023-12-30 13:17:30 +0900

    Refactoring part1

--- SphereController.js
+++ SphereController.js
@@ -746,17 +746,3 @@ var word_12 = [
     [-595, 750, 5],
   ],
 ];
-
-function normalize(v) {
-  var length = Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
-  // make sure we don't divide by 0.
-  if (length > 0.00001) {
-    return [v[0] / length, v[1] / length, v[2] / length];
-  } else {
-    return [0, 0, 0];
-  }
-}
-
-function subtractVectors(a, b) {
-  return [a[0] - b[0], a[1] - b[1], a[2] - b[2]];
-}
