05aaadf1f0570ace996f54bd8f7b9cb0d971e700
Naomi <futureverever@gmail.com>
2024-01-30 22:18:50 +0900

    Change speed

--- CommonUsage.js
+++ CommonUsage.js
@@ -20,7 +20,7 @@ const RIGHT = 400;
 const FRONT = -700;
 const BACK = 2000;
 
-const MOUSE_CONTROLL_SPEED = 4;
+const MOUSE_CONTROLL_SPEED = 2;
 let CRITICAL_POINT = 200 / MOUSE_CONTROLL_SPEED;
 
 const Z = 0;
